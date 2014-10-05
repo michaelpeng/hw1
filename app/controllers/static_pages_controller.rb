ee0250345f9db9968697f71fbffe43027d298c2a
vagrant <vagrant@rails-dev-box>
2014-10-05 09:00:03 +0000

    Done

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,9 @@
 class StaticPagesController < ApplicationController
+	def about
+		@name = 'Michael Peng'
+		@years_old = 19
+		@major = 'EECS'
+		@animal = "cat. No wait, dog. Man, I really can't chose between them.
+		Bunnies are cute af too though!! Hmm...they're all great."
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   #get '/about', to: 'CONTROLLER#FUNCTION'
   #get '/user/new', to: 'CONTROLLER#FUNCTION', as: 'users'
-  get '/user/:id', to: 'users#show', as: 'user'
+  get '/about', to: 'static_pages#about'
+  get '/user/new', to: 'users#new', as: 'users'
   post '/user/new', to: 'users#create'
+  get '/user/:id', to: 'users#show', as: 'user'
+  root 'users#index'
 end
